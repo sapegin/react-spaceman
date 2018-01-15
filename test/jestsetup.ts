76c5b6bcd081144a95db1d335bf5454b7352410d
Artem Sapegin <artem@sapegin.ru>
2018-01-15 21:51:42 +0100

    Chore: Update deps

--- test/jestsetup.ts
+++ test/jestsetup.ts
@@ -3,8 +3,3 @@ import * as Adapter from 'enzyme-adapter-react-16';
 
 // React 16 Enzyme adapter
 configure({ adapter: new Adapter() });
-
-// Fail tests on any warning
-console.error = message => {
-	throw new Error(message);
-};
